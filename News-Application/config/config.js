830f197b6ea6f3654414feca3d6ce9a1ae28d60b
Gimo <kgimase@gmail.com>
2022-12-31 22:41:47 +0300

    news mobile app

---
+++ News-Application/config/config.js
@@ -0,0 +1,4 @@
+export const API_KEY = `2943a9634988450c9794e279d251b842`;
+export const endpoint = `https://newsapi.org/v2/top-headlines`;
+export const category = 'general';
+export const country = 'us'
